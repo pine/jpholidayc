ca488419db69df894a890a743fcdbf69a7075f6b
Pine Mizune <pinemz@gmail.com>
2016-12-13 21:26:17 +0900

    test(cache): add cache.cr tests

---
+++ spec/jpholiday/cache_spec.cr
@@ -0,0 +1,31 @@
+require "../spec_helper"
+
+describe JpHoliday::Cache do
+  describe "get" do
+    it "# nil" do
+      cache : MockedCache?
+
+      begin
+        cache = MockedCache.new("config")
+        cache.get.should eq(nil)
+      ensure
+        cache.try &.dispose
+      end
+    end
+  end
+
+  describe "set" do
+    it "# basic" do
+      cache : MockedCache?
+
+      begin
+        cache = MockedCache.new("config")
+        cache.set({ "2016-12-13" => true })
+        cache.get.try(&.["2016-12-13"]?).should eq(true)
+        cache.get.try(&.["2016-12-14"]?).should eq(nil)
+      ensure
+        cache.try &.dispose
+      end
+    end
+  end
+end
